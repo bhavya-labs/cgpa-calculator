2320a1a3d837d9bc47d662887cfb64bb0855c3ba
bhavya-labs <alakuntabhavya65@gmail.com>
2026-05-29 16:52:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function generateInputs() {
 
-  const count = document.getElementById("semesterCount").value;
+  const count = parseInt(document.getElementById("semesterCount").value);
   const sgpaInputs = document.getElementById("sgpaInputs");
 
   sgpaInputs.innerHTML = "";
@@ -17,100 +17,4 @@ function generateInputs() {
       >
     `;
   }
-}
-
-function calculateCGPA() {
-
-  const sgpas = document.querySelectorAll(".sgpa");
-
-  let total = 0;
-  let count = 0;
-
-  for (let input of sgpas) {
-
-    let value = parseFloat(input.value);
-
-    if (isNaN(value)) continue;
-
-    if (value < 0 || value > 10) {
-      alert("Please enter SGPA between 0 and 10");
-      return;
-    }
-
-    total += value;
-    count++;
-  }
-
-  let cgpa = 0;
-
-  if (count > 0) {
-    cgpa = total / count;
-  }
-
-  document.getElementById("result").innerText =
-    `CGPA: ${cgpa.toFixed(2)}`;
-}
-function calculateRequiredSGPA() {
-
-  const target = parseFloat(document.getElementById("targetCGPA").value);
-
-  if (isNaN(target) || target < 0 || target > 10) {
-    document.getElementById("requiredResult").innerText =
-      "Enter valid target CGPA (0 - 10)";
-    return;
-  }
-
-  const sgpas = document.querySelectorAll(".sgpa");
-
-  let total = 0;
-  let completed = 0;
-
-  for (let input of sgpas) {
-
-    let value = parseFloat(input.value);
-
-    if (isNaN(value)) continue;
-
-    if (value < 0 || value > 10) {
-      document.getElementById("requiredResult").innerText =
-        "SGPA must be between 0 and 10";
-      return;
-    }
-
-    total += value;
-    completed++;
-  }
-
-  const totalSem = parseInt(document.getElementById("semesterCount").value);
-
-  if (!totalSem || totalSem <= 0) {
-    document.getElementById("requiredResult").innerText =
-      "Please select number of semesters first";
-    return;
-  }
-
-  const remaining = totalSem - completed;
-
-  if (remaining <= 0) {
-    document.getElementById("requiredResult").innerText =
-      "No remaining semesters left!";
-    return;
-  }
-
-  const requiredSGPA = ((target * totalSem) - total) / remaining;
-
-  if (requiredSGPA > 10) {
-    document.getElementById("requiredResult").innerText =
-      `Not possible 😓 You need ${requiredSGPA.toFixed(2)} SGPA (>10)`;
-    return;
-  }
-
-  if (requiredSGPA < 0) {
-    document.getElementById("requiredResult").innerText =
-      "Target already achieved 🎉";
-    return;
-  }
-
-  document.getElementById("requiredResult").innerText =
-    `You need average SGPA of ${requiredSGPA.toFixed(2)} in remaining semesters.`;
 }
